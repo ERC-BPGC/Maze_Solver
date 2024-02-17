e9211bee517498486b8921e73c7e99a938e1606d
indrajit-mandal <102886330+Jitmandal051004@users.noreply.github.com>
2024-02-17 06:54:39 +0530

    added delay for more controlled movement

--- sketch_jan24a.ino
+++ sketch_jan24a.ino
@@ -2,8 +2,13 @@ int t;
 
 const int LFOR = 11;
 const int LBACK = 10;
-const int RFOR = 13;
-const int RBACK = 12;
+const int RFOR = 9;
+const int RBACK = 6;
+
+//delay timing
+const int Spin_D = 215;
+const int Line_D = 315;
+const int Orient_D = 80;
 
 
 void setup() {
@@ -29,6 +34,21 @@ if(Serial.available()){
 
   digitalWrite(LBACK,LOW);
   digitalWrite(RBACK,LOW);
+
+  delay(Line_D);
+
+  digitalWrite(RFOR,HIGH);
+  digitalWrite(LBACK,HIGH);
+
+  digitalWrite(LFOR,LOW);
+  digitalWrite(RBACK,LOW);
+
+  delay(Orient_D);
+
+  digitalWrite(LFOR,LOW);
+  digitalWrite(LBACK,LOW);
+  digitalWrite(RFOR,LOW);
+  digitalWrite(RBACK,LOW);
 }
 
 
@@ -39,22 +59,43 @@ if(Serial.available()){
 
   digitalWrite(LFOR,LOW);
   digitalWrite(RFOR,LOW);
+
+  delay(Line_D);
+
+  digitalWrite(LFOR,LOW);
+  digitalWrite(LBACK,LOW);
+  digitalWrite(RFOR,LOW);
+  digitalWrite(RBACK,LOW);
 }
   
-else if(t == 76){      //turn LEFT (right side motors rotate in forward direction,  right side motors doesn't rotate)
+else if(t == 76){      //turn LEFT (right side motors rotate in forward direction,  left side motors rotate backward)
   digitalWrite(RFOR,HIGH);
+  digitalWrite(LBACK,HIGH);
 
   digitalWrite(LFOR,LOW);
   digitalWrite(RBACK,LOW);
-  digitalWrite(LBACK,LOW);
-}   
 
-else  if(t == 82){      //turn RT (right side motors rotate in forward direction, left  side motors doesn't rotate)
-  digitalWrite(LFOR,HIGH);
+  delay(Spin_D);
 
-  digitalWrite(RBACK,LOW);
+  digitalWrite(LFOR,LOW);
   digitalWrite(LBACK,LOW);
   digitalWrite(RFOR,LOW);
+  digitalWrite(RBACK,LOW);
+}   
+
+else  if(t == 82){      //turn RT (left side motors rotate in forward direction,  right side motors rotate backward)
+  digitalWrite(LFOR,HIGH);
+  digitalWrite(RBACK,HIGH);
+  
+  digitalWrite(LBACK,LOW);
+  digitalWrite(RFOR,LOW);
+
+  delay(Spin_D);
+
+  digitalWrite(LFOR,LOW);
+  digitalWrite(LBACK,LOW);
+  digitalWrite(RFOR,LOW);
+  digitalWrite(RBACK,LOW);
 }
 
 else if(t ==83){
