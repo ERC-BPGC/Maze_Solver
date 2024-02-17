b4c4f599d337320dbe91ba2644e06f07523388b3
anirband1 <61051946+anirband1@users.noreply.github.com>
2024-02-17 12:04:09 +0530

    Add files via upload

--- maze chart.py
+++ maze chart.py
@@ -2,6 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 PIXEL_SIZE = 144
+THRESHOLD = 0.3
+
+
+def rgb2gray(img):
+    height, width, _ = img.shape
+    gray_img_arr = np.zeros(shape=(height, width))
+
+    for i in range(height):
+        for j in range(width):
+            r_gray = img.item((i, j, 0)) * 0.299
+            g_gray = img.item((i, j, 1)) * 0.587
+            b_gray = img.item((i, j, 2)) * 0.114
+
+            gray_img_arr[i, j] = r_gray + g_gray + b_gray
+
+    return gray_img_arr
+
+def threshold(img, threshold):
+    height, width = img.shape
+
+    for i in range(height):
+        for j in range(width):
+            img[i, j] = 1 if img[i, j] > threshold else 0
+    
+    return img
 
 
 
@@ -24,6 +49,9 @@ img = plt.imread("abclol.jpeg")
 img = img/255
 height, width, _ = img.shape
 
-resized_image = scale_img2factor(img, 1/PIXEL_SIZE)
+final_img = scale_img2factor(img, 1/PIXEL_SIZE)
+final_img = rgb2gray(final_img)
+plt.imsave("idekbruh.jpeg", final_img, cmap = 'gray')
+final_img_t = threshold(final_img, THRESHOLD)
 
-plt.imsave("idekbruh.jpeg", resized_image)
+plt.imsave("idekbruht.jpeg", final_img_t, cmap = 'gray')
